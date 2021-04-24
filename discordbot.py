447e0795a2d9eb17e82fa3c357d12a99b235ea53
Tsutino <83033004+Tsutino@users.noreply.github.com>
2021-04-25 01:56:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -18,7 +18,7 @@ async def slot(ctx):
 
   kakuritu = random.randint(1, 319)
 
-  slot_list = ['<:element_tsutinoko:793148122653392937>', '<:habu:829971281754718240>', '<:resplendentquetzal:803594155451482113>', '<:prairiedog:793153927595163691>', ':aublack:']
+  slot_list = ['<:element_tsutinoko:793148122653392937>', '<:habu:829971281754718240>', '<:resplendentquetzal:803594155451482113>', '<:prairiedog:793153927595163691>', '<:ruter:835556735791661056>','<:dolca:793155035902640170>','<:aardwolf:793155951381184601>']
 
   A = random.choice(slot_list)
 
