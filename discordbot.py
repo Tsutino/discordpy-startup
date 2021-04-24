f948f1e42cb777492912c1ba787eaf6af0d2550d
Tsutino <83033004+Tsutino@users.noreply.github.com>
2021-04-25 01:26:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,24 +4,46 @@ import os
 
 import traceback
 
+import random
+
+import asyncio #sleepを使うのに必要
+
 bot = commands.Bot(command_prefix='/')
 
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-
-async def on_command_error(ctx, error):
-
-    orig_error = getattr(error, "original", error)
-
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-
-    await ctx.send(error_msg)
-
 @bot.command()
 
-async def ping(ctx):
+async def slot(ctx):
 
-    await ctx.send('pong')
+  kakuritu = random.randint(1, 2)
+
+  slot_list = [':aured:', ':augreen:', ':aublue:', ':auyellow:', ':aublack:']
+
+  A = random.choice(slot_list)
+
+  B = random.choice(slot_list)
+
+  C = random.choice(slot_list)
+
+  if int(kakuritu) == int(1): #確率は1 /399
+
+    await ctx.send("ボーナス確定！！！")
+
+    await asyncio.sleep(3) #3秒間待ってやる
+
+    i=0
+
+    while i < random.randint(1, 5):
+
+      await ctx.send("%s%s%s" % ("<:auyellow:824832834005893120>", "<:auyellow:824832834005893120>", "<:auyellow:824832834005893120>")) #7だけ出るように指定
+
+      await asyncio.sleep(1)
+
+      i += 1
+
+  else:
+
+    await ctx.send("%s%s%s" % (A, B, C))
 
 bot.run(token)
