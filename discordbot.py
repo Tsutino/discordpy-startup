d84e9b30128f4542c307014328fc8650f01a4340
tsutino <rinden.1118@gmail.com>
2021-04-26 23:56:17 +0900

    ボーナスを削除

--- discordbot.py
+++ discordbot.py
@@ -16,7 +16,6 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 async def slot(ctx):
 
-  kakuritu = random.randint(1, 319)
 
   slot_list = ['<:element_tsutinoko:793148122653392937>', '<:habu:829971281754718240>', '<:resplendentquetzal:803594155451482113>', '<:prairiedog:793153927595163691>', '<:ruter:835556735791661056>','<:dolca:793155035902640170>','<:aardwolf:793155951381184601>']
 
