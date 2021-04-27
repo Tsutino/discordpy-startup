6d9517328b1f836a88eb5b0074d88660e8394b65
sarukiti <sarukiti1891@gmail.com>
2021-04-27 14:48:06 +0900

    余分な行間を削除 スロットの抽選候補の読み込み位置を最適化

--- discordbot.py
+++ discordbot.py
@@ -1,31 +1,22 @@
 from discord.ext import commands
-
 import os
-
 import traceback
-
 import random
-
 import asyncio #sleepを使うのに必要
 
 bot = commands.Bot(command_prefix='/')
-
 token = os.environ['DISCORD_BOT_TOKEN']
+slot_list = ['<:element_tsutinoko:793148122653392937>', '<:habu:829971281754718240>', '<:resplendentquetzal:803594155451482113>', '<:prairiedog:793153927595163691>', '<:ruter:835556735791661056>','<:dolca:793155035902640170>','<:aardwolf:793155951381184601>']
 
 @bot.command()
-
 async def slot(ctx):
-
-
-  slot_list = ['<:element_tsutinoko:793148122653392937>', '<:habu:829971281754718240>', '<:resplendentquetzal:803594155451482113>', '<:prairiedog:793153927595163691>', '<:ruter:835556735791661056>','<:dolca:793155035902640170>','<:aardwolf:793155951381184601>']
-
+  #抽選
   A = random.choice(slot_list)
-
   B = random.choice(slot_list)
-
   C = random.choice(slot_list)
 
   await ctx.send("%s%s%s" % (f"{A} ",f"{B} ",f"{C} "))
+  #揃った場合
   if A==B==C:
     await ctx.send("スリーフレンズ！！！")
 
