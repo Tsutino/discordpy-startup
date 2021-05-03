88a956503cb3b25ac994d020e34db7f400a82692
sarukiti <sarukiti1891@gmail.com>
2021-05-04 03:37:07 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -42,6 +42,7 @@ async def slot(ctx):
     setTable(userID,0)
   #揃った場合
   if A==B==C:
+    await ctx.send(userID)
     await ctx.send("スリーフレンズ！！！")
     dict_result[userID] = int(dict_result[userID]) + 100
     updateTable((int(userID)),dict_result[userID])
