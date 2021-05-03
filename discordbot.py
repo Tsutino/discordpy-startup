7171c2b3bfaa33d07f8767035b8a6d2e8a48d68a
sarukiti <sarukiti1891@gmail.com>
2021-05-04 03:53:00 +0900

    頼むからこれで動いてくれ

--- discordbot.py
+++ discordbot.py
@@ -9,36 +9,29 @@ import psycopg2.extras
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 url = os.environ['DATABASE_URL']
-conn = psycopg2.connect(url)
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-cur.execute("""select * from discordbot_table;""")
-r = cur.fetchall()
-dict_result={dict(row)["ids"] : dict(row)["coins"] for row in r}
-cur.close()
-conn.close()
 
-def setTable(userID, coinNumber):
+def setDictionary():
   conn = psycopg2.connect(url)
   cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+  cur.execute("""select * from discordbot_table;""")
+  r = cur.fetchall()
+  dict_result={dict(row)["ids"] : dict(row)["coins"] for row in r}
+  return dict_result
+
+def setTable(userID, coinNumber):
   cur.execute("INSERT INTO discordbot_table (ids, coins) VALUES (%s, %s);", (userID, coinNumber))
   cur.execute("SELECT * FROM discordbot_table;")
   cur.fetchone()
   conn.commit()
-  cur.close()
-  conn.close()
 
 def updateTable(userID, coinNumber):
-  conn = psycopg2.connect(url)
-  cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
   cur.execute("UPDATE discordbot_table SET coins = '%s' WHERE ids='%s';", (coinNumber , userID))
   cur.execute("SELECT * FROM discordbot_table;")
   cur.fetchone()
   conn.commit()
-  cur.close()
-  conn.close()
 
 slot_list = ['<:element_tsutinoko:793148122653392937>', '<:aardwolf:793155951381184601>']
-
+dict_result = setDictionary()
 @bot.command()
 async def slot(ctx):
   global dict_result
@@ -55,6 +48,6 @@ async def slot(ctx):
     await ctx.send(userID)
     await ctx.send("スリーフレンズ！！！")
     dict_result[userID] = int(dict_result[userID]) + 100
+    dict_result = setDictionary()
     updateTable((int(userID)),dict_result[userID])
-
 bot.run(token)
