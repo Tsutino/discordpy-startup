aad2070d953b487657c26b9a87ad64359a34c78a
Tsutino <83033004+Tsutino@users.noreply.github.com>
2021-05-04 20:43:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -77,7 +77,7 @@ async def loop():
     # 現在の時刻
     now = datetime.now().strftime('%H:%M')
     dt = datetime.now().weekday()
-    if now == '20:38' and dt == 1:
+    if now == '20:45' and dt == 1:
       　channel = bot.get_channel(CHANNEL_ID)
         await channel.send('今日は道場越し！')  
 
